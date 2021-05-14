9b1b8cdb0b541547d6bcf2061e84de81e70541ba
saiminhtet <minhtet_sai@MIS200100025.NECSAPSIN.COM>
2021-05-14 17:35:52 +0800

    initial commit

---
+++ first/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Item extends React.Component {
+  render() {
+    return (
+      <li>
+        {this.props.name},
+        ${this.props.price}
+      </li>
+    );
+  }
+}
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>Hello React</h1>
+        <ul>
+          <Item name="Apple" price="0.99" />
+          <Item name="Orange" price="0.89"/>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App;
